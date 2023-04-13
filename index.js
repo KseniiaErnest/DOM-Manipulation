500bacad645936db27e19d174302c07f476ebdb0
Kseniia Ernest <hramtcovakseniia@gmail.com>
2023-04-13 12:18:28 -0400

    Start

---
+++ index.js
@@ -0,0 +1,31 @@
+console.log('INDEX.JS connected');
+
+// ****************************** part 1 ******************************
+
+
+document.querySelector('#new-task-form').addEventListener('submit', e => {
+  e.preventDefault();
+
+  const userInput = document.querySelector('#task-input');
+  const userName = document.querySelector('#owner-input');
+  
+  userInput.innerText = userInput.value;
+  
+  // 1: get the input from user related to the new task and the owner of the task
+  
+  // 2: append it to the DOM
+  
+  // 3: clear the form after the submission
+  
+  // ... your code here
+  
+});
+
+// ****************************** part 2 ******************************
+
+// 1: get the DOM element that represents 'Update gif' button
+// 2: add to click handler to it
+// 3: get the DOM element that represents 'iframe tag'
+// 4. change the attribute source to point to 'https://giphy.com/embed/1MTLxzwvOnvmE' after clicking on the 'Update gif' button
+
+// 5: bonus: make it so every time you  click on the 'Update gif' button, a different gif (one of these two) appears
